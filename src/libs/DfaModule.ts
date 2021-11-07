07613f97c013e8da8899b58c4bed5c3277821891
Safwan Shaheer <devorein00@gmail.com>
2021-11-07 22:14:09 +0600

    Added support for NOT operation

--- src/libs/DfaModule.ts
+++ src/libs/DfaModule.ts
@@ -113,60 +113,122 @@ export class DfaModule {
 		return errors;
 	}
 
-	merge(
-		dfaModule: DfaModule,
-		operation: 'and' | 'or',
-		mergedDfaOptions: Partial<Pick<Pick<IDfaModule, 'DFA'>['DFA'], 'label' | 'description'>>
+	#generateMergedDfaData(
+		dfaState: number | string,
+		newStates: string[],
+		newTransitions: IDfaModule['DFA']['transitions'],
+		newFinalStates: string[],
+		operation: 'or' | 'not' | 'and',
+		dfaModule?: DfaModule
+	) {
+		this.DFA.states.forEach((currentDfaState) => {
+			const newState = `${dfaState}${currentDfaState}`;
+			newStates.push(newState);
+			const newStateForSymbolZero =
+				(dfaModule ? dfaModule.DFA.transitions[dfaState][0].toString() : '') +
+				this.DFA.transitions[currentDfaState][0].toString();
+			const newStateForSymbolOne =
+				(dfaModule ? dfaModule.DFA.transitions[dfaState][1].toString() : '') +
+				this.DFA.transitions[currentDfaState][1].toString();
+			newTransitions[newState] = [newStateForSymbolZero, newStateForSymbolOne];
+			if (
+				operation === 'or' &&
+				((dfaModule ? dfaModule.DFA.final_states.includes(dfaState) : true) ||
+					this.DFA.final_states.includes(currentDfaState))
+			) {
+				newFinalStates.push(newState);
+			} else if (
+				operation === 'and' &&
+				(dfaModule ? dfaModule.DFA.final_states.includes(dfaState) : true) &&
+				this.DFA.final_states.includes(currentDfaState)
+			) {
+				newFinalStates.push(newState);
+			} else if (operation === 'not' && !this.DFA.final_states.includes(currentDfaState)) {
+				newFinalStates.push(newState);
+			}
+		});
+	}
+
+	#merge(
+		dfaModule: DfaModule | undefined,
+		operation: 'and' | 'or' | 'not',
+		mergedDfaOptions?: Partial<Pick<Pick<IDfaModule, 'DFA'>['DFA'], 'label' | 'description'>>
 	) {
 		const newStates: string[] = [];
 		const newTransitions: IDfaModule['DFA']['transitions'] = {};
-		const newStartState = dfaModule.DFA.start_state.toString() + this.DFA.start_state.toString();
+		const newStartState =
+			(dfaModule ? dfaModule.DFA.start_state.toString() : '') + this.DFA.start_state.toString();
 		const newFinalStates: string[] = [];
 
-		dfaModule.DFA.states.forEach((dfaState) => {
-			this.DFA.states.forEach((currentDfaState) => {
-				const newState = `${dfaState}${currentDfaState}`;
-				newStates.push(newState);
-				const newStateForSymbolZero =
-					dfaModule.DFA.transitions[dfaState][0].toString() +
-					this.DFA.transitions[currentDfaState][0].toString();
-				const newStateForSymbolOne =
-					dfaModule.DFA.transitions[dfaState][1].toString() +
-					this.DFA.transitions[currentDfaState][1].toString();
-				newTransitions[newState] = [newStateForSymbolZero, newStateForSymbolOne];
-				if (
-					operation === 'or' &&
-					(dfaModule.DFA.final_states.includes(dfaState) ||
-						this.DFA.final_states.includes(currentDfaState))
-				) {
-					newFinalStates.push(newState);
-				} else if (
-					operation === 'and' &&
-					dfaModule.DFA.final_states.includes(dfaState) &&
-					this.DFA.final_states.includes(currentDfaState)
-				) {
-					newFinalStates.push(newState);
-				}
+		if (dfaModule) {
+			dfaModule.DFA.states.forEach((dfaState) => {
+				this.#generateMergedDfaData(
+					dfaState,
+					newStates,
+					newTransitions,
+					newFinalStates,
+					operation,
+					dfaModule
+				);
 			});
-		});
+		} else {
+			this.#generateMergedDfaData(
+				'',
+				newStates,
+				newTransitions,
+				newFinalStates,
+				operation,
+				dfaModule
+			);
+		}
 
 		return new DfaModule(
 			(binaryString) => {
 				if (operation === 'or') {
-					return dfaModule.testLogic(binaryString) || this.testLogic(binaryString);
+					return (
+						(dfaModule ? dfaModule.testLogic(binaryString) : true) || this.testLogic(binaryString)
+					);
+				} else if (operation === 'and') {
+					return (
+						(dfaModule ? dfaModule.testLogic(binaryString) : true) && this.testLogic(binaryString)
+					);
 				} else {
-					return dfaModule.testLogic(binaryString) && this.testLogic(binaryString);
+					return !this.testLogic(binaryString);
 				}
 			},
 			{
 				final_states: newFinalStates,
-				label: mergedDfaOptions.label ?? `${dfaModule.DFA.label} - ${this.DFA.label}`,
+				label:
+					mergedDfaOptions?.label ??
+					`${dfaModule ? dfaModule.DFA.label + ' - ' : ''}${this.DFA.label}`,
 				description:
-					mergedDfaOptions.description ?? `${dfaModule.DFA.description} - ${this.DFA.description}`,
+					mergedDfaOptions?.description ??
+					operation.toUpperCase() +
+						'(' +
+						`${dfaModule ? dfaModule.DFA.description + ', ' : ''}${this.DFA.description}` +
+						')',
 				start_state: newStartState,
 				states: newStates,
 				transitions: newTransitions,
 			}
 		);
+	}
+
+	AND(
+		dfaModule: DfaModule,
+		mergedDfaOptions: Partial<Pick<Pick<IDfaModule, 'DFA'>['DFA'], 'label' | 'description'>>
+	) {
+		return this.#merge(dfaModule, 'and', mergedDfaOptions);
+	}
+
+	NOT(mergedDfaOptions: Partial<Pick<Pick<IDfaModule, 'DFA'>['DFA'], 'label' | 'description'>>) {
+		return this.#merge(undefined, 'not', mergedDfaOptions);
+	}
+
+	OR(
+		dfaModule: DfaModule,
+		mergedDfaOptions: Partial<Pick<Pick<IDfaModule, 'DFA'>['DFA'], 'label' | 'description'>>
+	) {
+		return this.#merge(dfaModule, 'or', mergedDfaOptions);
 	}
 }
