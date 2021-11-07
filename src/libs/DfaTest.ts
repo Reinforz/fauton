4addc9b8b8fba19b6c0d51dff43f2bdc66933d8e
Safwan Shaheer <devorein00@gmail.com>
2021-11-07 18:36:24 +0600

    Opt out of generating certain output files

--- src/libs/DfaTest.ts
+++ src/libs/DfaTest.ts
@@ -28,56 +28,85 @@ type IConfigs =
 			filePath: string;
 	  };
 
+interface IOutputFiles {
+	case: boolean;
+	incorrect: boolean;
+	correct: boolean;
+	input: boolean;
+	aggregate: boolean;
+}
+type IWriteStreams = Record<`${keyof IOutputFiles}WriteStream`, null | fs.WriteStream>;
+
 export class DfaTest {
 	#dfas: IDfaModule[];
 	#cliProgressBar: cliProgress.SingleBar;
-	logsPath: string;
+	#logsPath: string;
+	#outputFiles: IOutputFiles;
 
-	constructor(dfas: IDfaModule[], logsPath: string) {
+	constructor(dfas: IDfaModule[], logsPath: string, outputFiles: Partial<IOutputFiles>) {
 		this.#dfas = dfas;
+		this.#outputFiles = {
+			case: outputFiles.case ?? true,
+			aggregate: outputFiles.aggregate ?? true,
+			input: outputFiles.input ?? true,
+			correct: outputFiles.correct ?? true,
+			incorrect: outputFiles.incorrect ?? true,
+		};
 		this.#cliProgressBar = new cliProgress.SingleBar(
 			{
+				barsize: 20,
 				stopOnComplete: true,
-				format: colors.green('{bar}') + '| {percentage}% || {value}/{total} Chunks',
+				format: colors.green('{bar}') + ' {percentage}% {value}/{total} Chunks',
 			},
 			cliProgress.Presets.shades_classic
 		);
-		this.logsPath = logsPath;
+		this.#logsPath = logsPath;
 		if (!fs.existsSync(logsPath)) {
 			fs.mkdirSync(logsPath);
 		}
 	}
 
 	#createFileWriteStreams(dfaLabel: string) {
-		const generatedStreams = [
-			`${dfaLabel}.txt`,
-			`${dfaLabel}.incorrect.txt`,
-			`${dfaLabel}.correct.txt`,
-			`${dfaLabel}.input.txt`,
-		].map((fileName) => fs.createWriteStream(path.resolve(this.logsPath, fileName)));
+		const writeStreams: IWriteStreams = {
+			caseWriteStream: this.#outputFiles.case
+				? fs.createWriteStream(path.resolve(this.#logsPath, `${dfaLabel}.case.txt`))
+				: null,
+			aggregateWriteStream: this.#outputFiles.case
+				? fs.createWriteStream(path.resolve(this.#logsPath, `${dfaLabel}.aggregate.txt`))
+				: null,
+			incorrectWriteStream: this.#outputFiles.incorrect
+				? fs.createWriteStream(path.resolve(this.#logsPath, `${dfaLabel}.incorrect.txt`))
+				: null,
+			correctWriteStream: this.#outputFiles.correct
+				? fs.createWriteStream(path.resolve(this.#logsPath, `${dfaLabel}.correct.txt`))
+				: null,
+			inputWriteStream: this.#outputFiles.input
+				? fs.createWriteStream(path.resolve(this.#logsPath, `${dfaLabel}.input.txt`))
+				: null,
+		};
 
 		return {
-			streams: generatedStreams,
+			writeStreams,
 			endStreams() {
-				generatedStreams.forEach((generatedStream) => generatedStream.end());
+				Object.values(writeStreams).forEach((writeStream) => {
+					if (writeStream) {
+						writeStream.end();
+					}
+				});
 			},
 		};
 	}
 
 	#testDfas(
 		dfaModuleInfos: IDfaModuleInfo[],
-		writeStreams: Array<fs.WriteStream[]>,
+		writeStreams: Array<IWriteStreams>,
 		binaryStrings: string[],
 		post?: (dfaModule: IDfaModule, dfaModuleIndex: number) => void
 	) {
 		this.#dfas.forEach((dfaModule, dfaModuleIndex) => {
 			// The log files are generated based on the label of the dfa
-			const [
-				dfaWriteStream,
-				dfaIncorrectStringsWriteStream,
-				dfaCorrectStringsWriteStream,
-				dfaInputStringsWriteStream,
-			] = writeStreams[dfaModuleIndex];
+			const { caseWriteStream, correctWriteStream, incorrectWriteStream, inputWriteStream } =
+				writeStreams[dfaModuleIndex];
 
 			const dfaModuleInfo = dfaModuleInfos[dfaModuleIndex];
 
@@ -92,20 +121,22 @@ export class DfaTest {
 					} else {
 						dfaModuleInfo.truePositives += 1;
 					}
-					dfaCorrectStringsWriteStream.write(
-						dfaModuleResult + ' ' + logicTestResult + ' ' + binaryString + ' ' + '\n'
-					);
+					correctWriteStream &&
+						correctWriteStream.write(
+							dfaModuleResult + ' ' + logicTestResult + ' ' + binaryString + ' ' + '\n'
+						);
 				} else {
 					if (dfaModuleResult && !logicTestResult) {
 						dfaModuleInfo.falsePositives += 1;
 					} else {
 						dfaModuleInfo.falseNegatives += 1;
 					}
-					dfaIncorrectStringsWriteStream.write(
-						dfaModuleResult + ' ' + logicTestResult + binaryString + '\n'
-					);
+					incorrectWriteStream &&
+						incorrectWriteStream.write(
+							dfaModuleResult + ' ' + logicTestResult + ' ' + binaryString + ' ' + '\n'
+						);
 				}
-				dfaInputStringsWriteStream.write(binaryString + '\n');
+				inputWriteStream && inputWriteStream.write(binaryString + '\n');
 
 				const { withoutColors } = generateCaseMessage(
 					isWrong,
@@ -113,7 +144,7 @@ export class DfaTest {
 					dfaModuleResult,
 					logicTestResult
 				);
-				dfaWriteStream.write(withoutColors + '\n');
+				caseWriteStream && caseWriteStream.write(withoutColors + '\n');
 				this.#cliProgressBar.increment(1);
 			}
 			post && post(dfaModule, dfaModuleIndex);
@@ -124,12 +155,12 @@ export class DfaTest {
 		dfaModule: IDfaModule,
 		dfaModuleInfo: IDfaModuleInfo,
 		dfaModuleWriteStreams: {
-			streams: fs.WriteStream[];
+			writeStreams: IWriteStreams;
 			endStreams(): void;
 		}
 	) {
 		const {
-			streams: [dfaWriteStream],
+			writeStreams: { aggregateWriteStream },
 			endStreams,
 		} = dfaModuleWriteStreams;
 		const { withoutColors, withColors } = generateAggregateMessage(
@@ -142,7 +173,7 @@ export class DfaTest {
 		);
 
 		console.log('\n' + withColors);
-		dfaWriteStream.write(withoutColors);
+		aggregateWriteStream && aggregateWriteStream.write(withoutColors);
 
 		endStreams();
 	}
@@ -169,7 +200,7 @@ export class DfaTest {
 				const binaryStrings = chunks.toString().split('\n') as string[];
 				this.#testDfas(
 					dfaModuleInfos,
-					writeStreams.map(({ streams }) => streams),
+					writeStreams.map(({ writeStreams }) => writeStreams),
 					binaryStrings
 				);
 			}
@@ -193,7 +224,7 @@ export class DfaTest {
 			});
 			this.#testDfas(
 				dfaModuleInfos,
-				writeStreams.map(({ streams }) => streams),
+				writeStreams.map(({ writeStreams }) => writeStreams),
 				binaryStrings,
 				(dfaModule, dfaModuleIndex) => {
 					this.#postTest(dfaModule, dfaModuleInfos[dfaModuleIndex], writeStreams[dfaModuleIndex]);
