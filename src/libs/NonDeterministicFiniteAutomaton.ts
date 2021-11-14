c3a5b0579dddb4966d0d733f24c2cb59a748f773
Safwan Shaheer <devorein00@gmail.com>
2021-11-14 12:08:33 +0600

    Added nfa/ε-nfa to dfa conversion method

--- src/libs/NonDeterministicFiniteAutomaton.ts
+++ src/libs/NonDeterministicFiniteAutomaton.ts
@@ -1,4 +1,5 @@
 import { InputFiniteAutomaton } from '../types';
+import { DeterministicFiniteAutomaton } from './DeterministicFiniteAutomaton';
 import { FiniteAutomaton } from './FiniteAutomaton';
 
 export class NonDeterministicFiniteAutomaton extends FiniteAutomaton {
@@ -21,6 +22,8 @@ export class NonDeterministicFiniteAutomaton extends FiniteAutomaton {
 				if (epsilonTransitions![transitionState]) {
 					const firstPhaseSet = this.epsilonClosureOfState(transitionState);
 					const secondPhaseSet = new Set<string>();
+					const thirdPhaseSet: Set<string> = new Set();
+
 					firstPhaseSet.forEach((state) => {
 						// Some states might be null, and thus have no transitions
 						if (transitions[state]) {
@@ -30,7 +33,6 @@ export class NonDeterministicFiniteAutomaton extends FiniteAutomaton {
 						}
 					});
 
-					const thirdPhaseSet: Set<string> = new Set();
 					secondPhaseSet.forEach((secondPhaseState) => {
 						this.epsilonClosureOfState(secondPhaseState).forEach((state) => {
 							thirdPhaseSet.add(state);
@@ -46,7 +48,7 @@ export class NonDeterministicFiniteAutomaton extends FiniteAutomaton {
 
 	/**
 	 * Generates a set of states reachable from input state, using depth-first-search algorithm
-	 * @param state Start state
+	 * @param state state from where to start searching for epsilon closure states
 	 * @returns A set of states reachable from the input state on all epsilon transitions
 	 */
 	epsilonClosureOfState(state: string) {
@@ -64,10 +66,79 @@ export class NonDeterministicFiniteAutomaton extends FiniteAutomaton {
 			});
 		}
 
-		return allEpsilonStates;
+		return Array.from(allEpsilonStates);
+	}
+
+	// ε-closure(Move_NFA(states, letter))
+	moveAndEpsilonClosureStateSet(states: string[], symbol: string) {
+		const transitionRecordExtractedStates: Set<string> = new Set();
+		states.forEach((state) => {
+			const statesForSymbol = this.automaton.transitions[state]?.[symbol];
+			if (statesForSymbol) {
+				statesForSymbol.forEach((stateForSymbol) =>
+					transitionRecordExtractedStates.add(stateForSymbol)
+				);
+			}
+		});
+		const finalStates: Set<string> = new Set(states);
+		// No need to calculate epsilon closures of regular nfa
+		if (!this.automaton.epsilon_transitions) {
+			transitionRecordExtractedStates.forEach((transitionRecordExtractedState) => {
+				const epsilonClosureOfStates = this.epsilonClosureOfState(transitionRecordExtractedState);
+				epsilonClosureOfStates.forEach((epsilonClosureOfState) => {
+					finalStates.add(epsilonClosureOfState);
+				});
+			});
+		}
+		return Array.from(finalStates);
 	}
 
 	convertToDeterministicFiniteAutomaton() {
-		return null;
+		const startState = this.automaton.start_state;
+		const newStartStates = this.epsilonClosureOfState(startState);
+		const newStateStateString = newStartStates.sort().join(',');
+		const newStates: Set<string> = new Set();
+		const unmarkedStates: string[] = [newStateStateString];
+		const newTransitionsRecord: FiniteAutomaton['automaton']['transitions'] = {};
+		const totalAlphabets = this.automaton.alphabets.length;
+		const newFinalStates: Set<string> = new Set();
+
+		newStates.add(newStateStateString);
+
+		while (unmarkedStates.length !== 0) {
+			const currentStatesString = unmarkedStates.shift()!;
+			this.automaton.alphabets.forEach((symbol, symbolIndex) => {
+				const newStateString = this.moveAndEpsilonClosureStateSet(
+					currentStatesString!.split(','),
+					symbol
+				)
+					.sort()
+					.join(',');
+				if (!newStates.has(newStateString)) {
+					newStates.add(newStateString);
+					unmarkedStates.push(newStateString);
+				}
+				if (!newTransitionsRecord[currentStatesString]) {
+					newTransitionsRecord[currentStatesString] = new Array(totalAlphabets).fill(null) as any;
+				}
+				newTransitionsRecord[currentStatesString][symbolIndex] = [newStateString];
+				// Checking if the new state string should be a final state
+				this.automaton.final_states.forEach((finalState) => {
+					if (newStateString.includes(finalState)) {
+						newFinalStates.add(newStateString);
+					}
+				});
+			});
+		}
+
+		return new DeterministicFiniteAutomaton(this.testLogic, {
+			alphabets: this.automaton.alphabets,
+			final_states: Array.from(newFinalStates),
+			label: this.automaton.label,
+			start_state: newStateStateString,
+			states: Array.from(newStates),
+			transitions: newTransitionsRecord,
+			epsilon_transitions: null,
+		});
 	}
 }
