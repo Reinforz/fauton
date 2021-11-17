514891a160bebaf7075b6bf69d71be8d24fcb18c
Safwan Shaheer <devorein00@gmail.com>
2021-11-17 09:07:01 +0600

    MISC: Formatted all src files

--- src/utils/generateCaseMessage.ts
+++ src/utils/generateCaseMessage.ts
@@ -7,19 +7,17 @@ export default function generateCaseMessage(
 	logicTestResult: boolean
 ) {
 	return {
-		withColors:
-			`${[
-				`Result: ${  isWrong ? colors.red.bold(`WRONG`) : colors.green.bold(`CORRECT`)}`,
-				`String: ${  colors.yellow.bold(randomInputString)}`,
-				`Logic: ${  colors.blue.bold(logicTestResult.toString())}`,
-				`DFA: ${  colors.blue.bold(dfaTestResult.toString())}`,
-			].join('\n')  }\n`,
-		withoutColors:
-			`${[
-				`Result: ${  isWrong ? `WRONG` : `CORRECT`}`,
-				`String: ${  randomInputString}`,
-				`Logic: ${  logicTestResult}`,
-				`DFA: ${  dfaTestResult}`,
-			].join('\n')  }\n`,
+		withColors: `${[
+			`Result: ${isWrong ? colors.red.bold(`WRONG`) : colors.green.bold(`CORRECT`)}`,
+			`String: ${colors.yellow.bold(randomInputString)}`,
+			`Logic: ${colors.blue.bold(logicTestResult.toString())}`,
+			`DFA: ${colors.blue.bold(dfaTestResult.toString())}`,
+		].join('\n')}\n`,
+		withoutColors: `${[
+			`Result: ${isWrong ? `WRONG` : `CORRECT`}`,
+			`String: ${randomInputString}`,
+			`Logic: ${logicTestResult}`,
+			`DFA: ${dfaTestResult}`,
+		].join('\n')}\n`,
 	};
 }
