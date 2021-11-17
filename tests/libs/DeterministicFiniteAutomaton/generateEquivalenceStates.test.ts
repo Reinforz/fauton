32034ac49694d4282fb9c5f803775869d167e1e1
Safwan Shaheer <devorein00@gmail.com>
2021-11-17 16:19:44 +0600

    TEST: Attained 100% coverage for DeterministicFiniteAutomaton.generateEquivalenceStates module

---
+++ tests/libs/DeterministicFiniteAutomaton/generateEquivalenceStates.test.ts
@@ -0,0 +1,51 @@
+import { checkEquivalenceBetweenStatesGroups } from '../../../src/libs/DeterministicFiniteAutomaton/utils/checkEquivalenceBetweenStatesGroups';
+import { generateEquivalenceStates } from '../../../src/libs/DeterministicFiniteAutomaton/utils/generateEquivalenceStates';
+
+it(`Should work`, () => {
+	expect(
+		checkEquivalenceBetweenStatesGroups([
+			generateEquivalenceStates(
+				{
+					alphabets: ['0', '1'],
+					states: ['0', '1', '2', '3', '4', '5', '6', '7'],
+					transitions: {
+						0: {
+							0: ['1'],
+							1: ['5'],
+						},
+						1: {
+							0: ['6'],
+							1: ['2'],
+						},
+						2: {
+							0: ['0'],
+							1: ['2'],
+						},
+						3: {
+							0: ['2'],
+							1: ['6'],
+						},
+						4: {
+							0: ['7'],
+							1: ['5'],
+						},
+						5: {
+							0: ['2'],
+							1: ['6'],
+						},
+						6: {
+							0: ['6'],
+							1: ['4'],
+						},
+						7: {
+							0: ['6'],
+							1: ['2'],
+						},
+					},
+				},
+				[['0', '1', '3', '4', '5', '6', '7'], ['2']]
+			),
+			[['0', '4', '6'], ['1', '7'], ['3', '5'], ['2']],
+		])
+	).toStrictEqual(true);
+});
