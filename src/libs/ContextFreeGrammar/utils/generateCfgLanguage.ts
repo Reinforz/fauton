1182dfe42494eb0e244f7040b2673e35747e30b8
Safwan Shaheer <devorein00@gmail.com>
2021-12-20 12:56:09 +0600

    Fixed generateCfgLanguage tests

--- src/libs/ContextFreeGrammar/utils/generateCfgLanguage.ts
+++ src/libs/ContextFreeGrammar/utils/generateCfgLanguage.ts
@@ -1,29 +1,45 @@
 import { LinkedList } from '@datastructures-js/linked-list';
 import { CFGOption } from '../../../types';
 import { simplifyCfg } from './simplifyCfg';
+import { validateCfg } from './validateCfg';
 
 interface IQueueItem {
 	path: string[];
 	label: string;
 	rules: Array<[string, number]>;
 	chunks: string[];
-	word: string;
+	sentence: string;
 }
 
 /**
  * Generates all the strings of a given cfg within certain length along with the path taken to generate them
- * @param cfgOptions Variables, terminals and transition Record for the cfg
- * @param maxChunkLength Maximum length of the generated string
+ * @param cfgOptions Variables and transition Record for the cfg
+ * @param maxLength Maximum length of the generated string
  * @param skipSimplification Should the cfg simplification process be skipped, useful if you already have a simplified cfg, and dont want to spend additional computational power behind it, also sometimes you dont want to simplify cfg as it updates the production rules
  * @returns A record of generated string and the path taken to generate them
  */
 export function generateCfgLanguage(
 	cfgOptions: CFGOption,
-	maxChunkLength: number,
-	skipSimplification?: boolean
+	options: {
+		minLength: number;
+		maxLength: number;
+		skipSimplification?: boolean;
+		skipValidation?: boolean;
+		generateTerminals?: boolean;
+	}
 ) {
 	const { productionRules, startVariable, variables } = cfgOptions;
+	const {
+		/* generateTerminals = false,  */ minLength,
+		maxLength,
+		skipSimplification = false,
+		skipValidation = false,
+	} = options;
+	if (!skipValidation) {
+		validateCfg(cfgOptions);
+	}
 	const simplifiedVariables = skipSimplification ? variables : simplifyCfg(cfgOptions);
+
 	const linkedList = new LinkedList<IQueueItem>();
 	// A set to keep track of all the words that have been traversed
 	const traversedSet = new Set(startVariable);
@@ -32,34 +48,38 @@ export function generateCfgLanguage(
 	linkedList.insertFirst({
 		path: [startVariable],
 		chunks: [startVariable],
-		word: startVariable,
+		sentence: startVariable,
 		label: startVariable,
 		rules: [],
 	});
 
 	const cfgLanguageRecord: Record<string, IQueueItem> = {};
 	// A set to keep track of all the strings of the language
-	const cfgLanguage: Set<string> = new Set();
+	const cfgSentences: Set<string> = new Set();
 
 	while (linkedList.count()) {
 		const queueItem = linkedList.removeFirst()!;
-		const { path, chunks, word, rules } = queueItem.getValue();
-		// Extracting all the variables of the word
+		const { path, chunks, sentence, rules } = queueItem.getValue();
+		// Extracting all the variables of the sentence
 
-		if (chunks.length <= maxChunkLength) {
+		if (chunks.length <= maxLength) {
 			const variablesInWord = chunks.filter((chunk) => variablesSet.has(chunk));
-			// If there are no variables, we've generated a concrete word of the language
-			if (variablesInWord.length === 0 && !cfgLanguageRecord[word]) {
-				cfgLanguageRecord[word] = {
+			// If there are no variables, we've generated a concrete sentence of the language
+			if (
+				variablesInWord.length === 0 &&
+				!cfgLanguageRecord[sentence] &&
+				chunks.length >= minLength
+			) {
+				cfgLanguageRecord[sentence] = {
 					path,
 					rules,
-					word,
+					sentence,
 					label: path.join(' -> '),
 					chunks,
 				};
-				cfgLanguage.add(word);
+				cfgSentences.add(sentence);
 			} else {
-				// Loop through each of the variables in the word
+				// Loop through each of the variables in the sentence
 				variablesInWord.forEach((variable) => {
 					productionRules[variable].forEach((substitution, substitutionIndex) => {
 						// Keeping a temporary chunk as we dont want to update the previous chunks
@@ -68,8 +88,8 @@ export function generateCfgLanguage(
 						// Variables to store left and right chunks after substituting
 						const leftChunks: string[] = [],
 							rightChunks: string[] = [];
-						// Replace the variable in the word with the substitution
-						// Find the variable index inside the word chunks
+						// Replace the variable in the sentence with the substitution
+						// Find the variable index inside the sentence chunks
 						const chunksVariableIndex = chunks.findIndex((chunk) => chunk === variable);
 						// If the Chunks contain the variable
 						if (chunksVariableIndex !== -1) {
@@ -83,7 +103,7 @@ export function generateCfgLanguage(
 							});
 						}
 						const substitutedWord = tempChunks.join(' ');
-						if (!traversedSet.has(substitutedWord) && tempChunks.length <= maxChunkLength) {
+						if (!traversedSet.has(substitutedWord) && tempChunks.length <= maxLength) {
 							const newPath = [
 								...path,
 								[...leftChunks, `(${substitution})`, ...rightChunks].join(' '),
@@ -92,7 +112,7 @@ export function generateCfgLanguage(
 							linkedList.insertLast({
 								rules: [...rules, [variable, substitutionIndex]],
 								path: newPath,
-								word: substitutedWord,
+								sentence: substitutedWord,
 								label: newPath.join(' -> '),
 								chunks: tempChunks,
 							});
@@ -106,7 +126,7 @@ export function generateCfgLanguage(
 	// Returning the production rule as it might be different if we have removed null production rules
 	return {
 		tree: cfgLanguageRecord,
-		language: Array.from(cfgLanguage),
+		sentences: Array.from(cfgSentences),
 		productionRules,
 	};
 }
