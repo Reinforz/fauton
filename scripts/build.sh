cf6fc139ff07d511725efbc98589278a3d0dd412
Safwan Shaheer <devorein00@gmail.com>
2021-12-28 13:30:46 +0600

    Remove building example folder completely

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,17 +7,21 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+echo $GITHUB_WORKSPACE
+
 for package in "${packages[@]}" ; do
   package_name="@fauton/$package"
   cd "${GITHUB_WORKSPACE}/packages/$package"
-  echo -e "${BLUE}Building package $package_name${NC}"
+  tsc="${GITHUB_WORKSPACE}/node_modules/.bin/tsc"
+  jest="${GITHUB_WORKSPACE}/node_modules/.bin/jest"
+  eslint="${GITHUB_WORKSPACE}/node_modules/.bin/eslint"
 
-  if ! (npm install -g) then
-    echo -e "${RED}Error installing $package_name globally${NC}"
-    exit 1
-  else
-    echo -e "${GREEN}Successfully installed $package_name globally${NC}"
-  fi
+  # if ! (npm install -g) then
+  #   echo -e "${RED}Error installing $package_name globally${NC}"
+  #   exit 1
+  # else
+  #   echo -e "${GREEN}Successfully installed $package_name globally${NC}"
+  # fi
 
   if ! (npm install) then
     echo -e "${RED}Error installing $package_name dependencies${NC}"
@@ -26,28 +30,24 @@ for package in "${packages[@]}" ; do
     echo -e "${GREEN}Successfully installed $package_name dependencies${NC}"
   fi
 
-  if ! (npm run build) then
+  if ! (node $eslint ./libs --ext tsx,ts) then
+    echo -e "${RED}Error linting $package_name${NC}"
+    exit 1
+  else
+    echo -e "${GREEN}Successfully linted $package_name${NC}"
+  fi
+
+  if ! (node $tsc --sourceMap false) then
     echo -e "${RED}Error building $package_name${NC}"
     exit 1
   else
     echo -e "${GREEN}Successfully build $package_name${NC}"
   fi
 
-  if ! (npm run test) then
+  if ! (node $jest --runInBand) then
     echo -e "${RED}Error testing $package_name${NC}"
     exit 1
   else
     echo -e "${GREEN}Successfully tested $package_name${NC}"
-  fi
-  # If the examples directory exist for this package
-  if [ -d "./examples" ]; then
-    cd ./examples
-    npm link $package_name
-    if ! (npm run build) then
-      echo -e "${RED}Error building $package_name examples${NC}"
-      exit 1
-    else
-      echo -e "${GREEN}Successfully build $package_name examples${NC}"
-    fi
   fi
 done
