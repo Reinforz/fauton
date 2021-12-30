c975495b8bc06b1961dc5243292c56cd7e601d25
Safwan Shaheer <devorein00@gmail.com>
2021-12-30 19:09:58 +0600

    Added support for multi letter tokens when generating universal language

--- packages/testing/libs/utils/generateUniversalLanguage.ts
+++ packages/testing/libs/utils/generateUniversalLanguage.ts
@@ -1,36 +1,36 @@
 /**
- * Generates all combination of strings that can be made by the alphabet from length 1 to `maxLength`
- * @param alphabet Alphabet of the strings
- * @param maxLength Max length of the generated strings
- * @param startLength Starting length of the string
+ * Generates all combination of strings that can be made by the tokens from length 1 to `maxTokenLength`
+ * @param tokens Alphabet of the strings
+ * @param maxTokenLength Max length of the generated strings
+ * @param startTokenLength Starting length of the string
  * @param languageChecker A cb passed each generated string
  * @returns An array of strings
  */
 export function generateUniversalLanguage(
-	alphabet: string[],
-	maxLength: number,
-	startLength?: number,
+	tokens: string[],
+	maxTokenLength: number,
+	startTokenLength?: number,
 	// eslint-disable-next-line
-	cb?: (inputString: string) => void
+	cb?: (inputTokens: string[]) => void
 ) {
-	const generatedStrings: Array<string> = [];
+	const language: Array<Array<string>> = [];
 
-	function generateAllKLength(generatedString: string, stringLength: number) {
-		if (stringLength === 0) {
+	function generateAllKLength(generatedTokens: string[], tokensLength: number) {
+		if (tokensLength === 0) {
 			if (cb) {
-				cb(generatedString);
+				cb(generatedTokens);
 			}
-			generatedStrings.push(generatedString);
+			language.push(generatedTokens);
 			return;
 		}
-		for (let i = 0; i < alphabet.length; i += 1) {
-			generateAllKLength(generatedString + alphabet[i], stringLength - 1);
+		for (let i = 0; i < tokens.length; i += 1) {
+			generateAllKLength(generatedTokens.concat(tokens[i]), tokensLength - 1);
 		}
 	}
 
-	for (let length = startLength ?? 1; length <= maxLength; length += 1) {
-		generateAllKLength('', length);
+	for (let length = startTokenLength ?? 1; length <= maxTokenLength; length += 1) {
+		generateAllKLength([], length);
 	}
 
-	return generatedStrings;
+	return language;
 }
