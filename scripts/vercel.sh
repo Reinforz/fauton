5ceaff2930c8a60a35e0e488dcdc4699ab18dc7f
Safwan Shaheer <devorein00@gmail.com>
2022-01-26 20:34:50 +0600

    Added vercel.sh to skip preview builds

---
+++ scripts/vercel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "VERCEL_ENV: $VERCEL_ENV"
+
+if [[ "$VERCEL_ENV" == "production" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  # Don't build for preview
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
