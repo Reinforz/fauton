43422a26b21d44293fe43c1002f66d910d0028df
Safwan Shaheer <devorein00@gmail.com>
2022-02-26 11:43:09 +0600

    Added tests for generateRegexTree module

--- packages/regex/tests/generateRegexTree.test.ts
+++ packages/regex/tests/generateRegexTree.test.ts
@@ -1,12 +1,27 @@
-import { addConcatOperatorToRegex } from "../libs/utils/addConcatOperatorToRegex"
-import { convertInfixRegexToPostfix } from "../libs/utils/convertInfixRegexToPostfix"
-import { generateRegexTree } from "../libs/utils/generateRegexTree"
+import { addConcatOperatorToRegex } from '../libs/utils/addConcatOperatorToRegex';
+import { convertInfixRegexToPostfix } from '../libs/utils/convertInfixRegexToPostfix';
+import { generateRegexTree } from '../libs/utils/generateRegexTree';
 
 describe('generateRegexTree', () => {
-  it(`Simple regex`, () => {
-    const generatedRegexTree = generateRegexTree(addConcatOperatorToRegex(convertInfixRegexToPostfix("a|b?c*")))
-    expect(generatedRegexTree).toStrictEqual({
-      
-    })
-  })
-})
+	it(`Simple regex`, () => {
+		const generatedRegexTree = generateRegexTree(
+			convertInfixRegexToPostfix(addConcatOperatorToRegex('a|b?c*'))
+		);
+		expect(generatedRegexTree).toStrictEqual([
+			{
+				operands: [
+					{ operands: ['a'], operator: 'Literal' },
+					{
+						operands: [
+							{ operands: [{ operands: ['b'], operator: 'Literal' }], operator: undefined },
+							{ operands: [{ operands: ['c'], operator: 'Literal' }], operator: 'Kleene' },
+						],
+						operator: 'Concat',
+					},
+				],
+				operator: 'Or',
+			},
+			'',
+		]);
+	});
+});
