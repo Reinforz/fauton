8c6ca32f8117b12fae4ca607c13427bdaf80499a
Safwan Shaheer <devorein00@gmail.com>
2022-05-23 07:46:55 +0600

    Improved tests for leftFactoring

--- packages/cfg/tests/leftFactoring.test.ts
+++ packages/cfg/tests/leftFactoring.test.ts
@@ -1,7 +1,30 @@
 import { leftFactoring } from '../libs/leftFactoring';
 
 describe('leftFactoring', () => {
-	it(`Should be able to left factor for single production rule`, () => {
+	it(`Should be able to left factor for multiple production rule`, () => {
+		const leftRefactored = leftFactoring({
+      productionRules: {
+        S: ["a A d", "a B"],
+        A: ["a", "a b"],
+        B: ["c c d", "d d c"]
+      },
+      startVariable: "S"
+    });
+		expect(leftRefactored).toStrictEqual({
+			productionRules: {
+				S: ["a S'"],
+				"S'": ['A d', 'B'],
+				A: ["a A'"],
+				"A'": ['', 'b'],
+				B: ['c c d', 'd d c'],
+			},
+			startVariable: 'S',
+			variables: ['S', "S'", 'A', "A'", 'B'],
+			terminals: ['a', 'd', 'b', 'c'],
+		});
+	});
+
+  it(`Should be able to left factor for single production rule`, () => {
 		const leftRefactored = leftFactoring({
 			productionRules: {
 				S: ['b', 'a S S b S', 'a S a S b', 'a b b'],
